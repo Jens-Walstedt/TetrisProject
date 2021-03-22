b702745c11bac5e8bab3775cf18400e63576aedb
Jens Wålstedt <jens.walstedt@gmail.com>
2021-03-22 11:01:14 +0100

    uncommented stuff to work

--- Engine.h
+++ Engine.h
@@ -31,7 +31,7 @@ private:
 
 
 public:
-	//Engine();
+	Engine();
 	void start();
 };
 
--- Highscore.cpp
+++ Highscore.cpp
@@ -14,7 +14,7 @@ void Highscore::reset() {}
 
 void Highscore::update(const sf::Time& dt) {}
 
-int Highscore::getLvl() {
+int Highscore::getLvl() const {
 	int x = 1;
 	return x;
 }
