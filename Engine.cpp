cf895e9a88ea5a8cee4041f155d06fe4b1ae9998
Mattias Mousa <mattiasmousa@gmail.com>
2021-04-01 16:20:20 +0200

    updated fall speed for higher lvl

--- Engine.cpp
+++ Engine.cpp
@@ -46,7 +46,7 @@ Engine::Engine() :
     m_MenuWindow = std::make_unique<MenuWindow>(sf::Vector2f(m_GridPosition.x + 32, m_GridPosition.y + 32)
         , sf::Vector2f(m_Grid->GetWidth() - 64, m_FieldSize * 5), m_Font);
 
-    if (!m_Background.loadFromFile("tetrisbackground18.png")) {
+    if (!m_Background.loadFromFile("tetrisbackground1.png")) {
         std::cout << "Engine::Engine() - could not load m_Background\n";
     };
     m_BackgroundSprite.setTexture(m_Background);
@@ -66,7 +66,7 @@ void Engine::start()
     //GameLoop
     while (m_Window.isOpen() && Quit == false)
     {
-        sf::Time fallSpeed{ sf::seconds(85.f / (85.f + (m_HighScore.getLvl() * (m_HighScore.getLvl() * 5.f)))) };
+        sf::Time fallSpeed{ sf::seconds(200.f / (200.f + (m_HighScore.getLvl() * (m_HighScore.getLvl() * 30.f)))) };
         time = clock.restart();
         m_ElapsedTime += time;
         events();
